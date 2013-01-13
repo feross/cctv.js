d27e11b65b044c47078f6198c6cc6b88360a3c4c
Feross Aboukhadijeh <feross@feross.org>
2013-01-12 17:21:09 -0800

    add socket.io

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  // app.use(require('stylus').middleware(__dirname + '/static'));
+  app.use(express.static(path.join(__dirname, 'static')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+
+io.sockets.on('connection', function (socket) {
+
+  // socket.on('')
+
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+});
