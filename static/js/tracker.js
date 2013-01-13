e2621951e4cf19cff8183e8c39baf32d91529494
Feross Aboukhadijeh <feross@feross.org>
2013-01-13 03:52:36 -0800

    add simple event tracking and add more libraries to sample site

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -1,4 +1,27 @@
-var socket = io.connect('http://localhost');
+var socket = io.connect('http://localhost')
+  , THROTTLE_TIME = 500
+
+$(window).on('scroll', _.throttle(function (event) {
+  var top = $(window).scrollTop()
+    , left = $(window).scrollLeft()
+
+  log(top, left)
+}, THROTTLE_TIME))
+
+$(window).on('resize', _.throttle(function (event) {
+  var width = $(window).width()
+    , height = $(window).height()
+
+  log(width, height)
+}, THROTTLE_TIME))
+
+$(document).on('mousemove', _.throttle(function (event) {
+  var x = event.pageX
+    , y = event.pageY
+
+  log(x, y)
+}, 50))
+
 
 // collects initial info the page (size, scroll position)
 // socket.emit('update', {});
